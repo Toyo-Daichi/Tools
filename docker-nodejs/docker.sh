c070de9d8ab862590e47f3da743f2b0130f784d0
Toyo-Daichi <daichi.toyooka.gu@gmail.com>
2022-07-15 16:55:39 +0900

    add: nvm env.

--- docker-nodejs/docker.sh
+++ docker-nodejs/docker.sh
@@ -6,8 +6,8 @@
 #----------------------------------------------------------------------
 export AWS_PROFILE='amplify-On38'
 #
-image_name='nodejs/amplify'
-user_name='node'
+image_name='nodejs/nvm'
+user_name='docker-toyo'
 #
 statuses='create login'
 
@@ -22,7 +22,7 @@ select status in ${statuses}; do
       -u `id -u ${USER}` \
       -v ${HOME}/:/home/${user_name} -v ${HOME}/.aws/:/home/${user_name}/.aws \
       -e AWS_PROFILE -e AWS_DEFAULT_REGION \
-      -p 3000:3000 -p 8080:8080 -p 39895:39895 \
+      -p 3000:3000 -p 8080:8080 \
       ${image_name} /bin/bash --login 
   fi
   break
